931096d06ce5dcbe9c93e22676d58eee45abc4b2
meow <qaqmoss@gmail.com>
2023-02-05 22:37:35 +0800

    order moudle define dot h file over

---
+++ include/order/order.h
@@ -0,0 +1,46 @@
+//order.h
+#ifndef _ORDER_H_
+#define _ORDER_H_
+#include "config.h"
+typedef struct {
+    char id[MAX_LEN];
+    char good_id[MAX_LEN];
+    double price;
+    char date[MAX_LEN];
+    char seller_id[MAX_LEN];
+    char buyer_id[MAX_LEN];
+}Order;
+
+void pullOrders();
+/**
+ * @brief 从外部文件导入订单信息
+*/
+
+void pushOrders();
+/**
+ * @brief 从内部导出订单信息到外部文件
+*/
+
+int addOrder(Order* o);
+/**
+ * @brief 增加一个订单
+ * @param o 待加入的订单
+ * @return 成功返回1失败返回0
+*/
+
+void printOrders();
+/**
+ * @brief 打印所有的订单信息
+ * 
+*/
+void printOrders4Seller(const char* id);
+/**
+ * @brief 打印给出卖家id打印订单
+ * @param id 卖家id
+*/
+
+void printOrders4Buyer(const char* id);
+/**
+ * @brief 打印给出买家id的订单
+ * @param id 卖家买家
+*/
--- src/good/good.c
+++ src/good/good.c
@@ -1,5 +1,7 @@
 #include "good/good.h"
+#include "tools/info.h"
 #include <stdio.h>
+#include <string.h>
 
 static Good goods[MAX_GOOD];
 static int totalGood = 0;
@@ -14,5 +16,103 @@ void pullGoods() {
         &goods[totalGood].price, goods[totalGood].seller_id, &goods[totalGood].state, \
         goods[totalGood].date, goods[totalGood].description) != EOF) totalGood++;
         fclose(pf);
+    }
+}
+Good* getGood(int index) {
+    return goods + index;
+}
+
+static const char* stateName[] = {"Selling", "Sold", "Banned"};
+
+void goodInfo(int i) {
+    printf("ID:             | %s\n", goods[i].id);
+    printf("Name:           | %s\n", goods[i].description);
+    printf("Description:    | %s\n", goods[i].description);
+    printf("Date:           | %s\n", goods[i].date);
+    printf("Price:          | %.lf\n", goods[i].price);
+    printf("Seller:         | %s\n", goods[i].seller_id);
+    printf("State:          | %s\n", goods[i].state);
+}
+
+int addGood(Good* g) {
+    if (totalGood == MAX_GOOD) return 0;
+    genID(g->id, "G");
+    getDate(g->date);
+    g->state = SELLING;
+    goods[totalGood++] = *g;
+    return 1;
+}
+
+int searchGoodID(const char* id) {
+    for (int i = 0; i< totalGood; i++) {
+        if (strcmp(goods[i].id, id) == 0) return i;
+    return -1;
+    }
+}
+
+int deleteGood(const char* id, const char* who) {
+    int idx = searchGoodID(id);
+    if (idx == -1) return 0;
+    if (who && strcmp(goods[idx].seller_id, who)) return 0;
+    goods[idx].state = BANED;
+    return 1;
+}
+
+static const char* header = "|ID        |Name       |Price      |Date       |Seller     |State      |";
+static const char* divide = "+----------+-----------+-----------+-----------+-----------+-----------+";
+/**
+ * @brief 打印单个商品作为表格的一行
+ * @param i 用户下标
+*/
+static void printGood(int i) {
+    printf("|%-10s |%-10s |%-10.lf |%-10s |%-10s |%-10s |\n", goods[i].id, goods[i].name, \
+    goods[i].price, goods[i].date, goods[i].seller_id, goods[i].state);
+}
+
+void printGoods() {
+    print_header
+    for (int i = 0; i < totalGood; i++) {
+        printGood[i];
+        print_divide
+    }
+}
+
+void searchGoodName4Buyer(const char* name) {
+    print_header
+    for (int i = 0; i < totalGood; i++) {
+        if (good[i].state == SELLING && strstr(goods[i].name, name)) {
+            printGood(i);
+            print_divide
+        }
+    }
+}
+
+void searchGoodName4Admin(const char* name) {
+    print_header
+    for (int i = 0; i < totalGood; i++) {
+        if (strstr(goods[i].name, name)) {
+            printGood(i);
+            print_divide
+        }
+    }
+}
+
+void printGoods4Seller(const char* id) {
+    print_header
+    for (int i = 0; i < totalGood; i++) {
+        if(strcmp(goods[i].seller_id, id) == 0) {
+            printGood(i);
+            print_divide
+        } 
+    }
+}
+
+void searchGoodName4Buyer(const char* name, const char* name) {
+    print_header
+    for (int i = 0; i < totalGood; i++) {
+        if (strstr(goods[i].name, name)) {
+            printGood(i);
+            print_divide
+        }
     }
 }
